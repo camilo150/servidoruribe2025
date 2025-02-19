1114b0860e16a4fd0ae293269d16e21eb3fd89ad
camilo <camicastrillon2314@gmail.com>
2025-02-18 21:46:35 -0500

    se alade la tabla de proveedor

--- app/api/models/proveedorLogistica.py
+++ app/api/models/proveedorLogistica.py
@@ -9,3 +9,15 @@ class Proveedor():
     __tablename__='proveedores'
     nombres=Column(String(50))
     documento=Column(String(50))
+    direccion =Column(String(50))
+    ciudad= Column(String(50))
+    representante= Column(String(50))
+    telefono_Contacto= Column(String(50))
+    correo= Column(String(50))
+    costo_Envio= Column(String(50))
+    fecha_Envio= Column(Date)
+    descripcion= Column(String(50))
+    
+    
+class Logistica():
+    __tablename__=''
