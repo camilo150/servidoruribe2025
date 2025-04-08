7348fa7d7a091eed6e3eca9b945efba00d0317d3
camilo <camicastrillon2314@gmail.com>
2025-04-07 20:34:29 -0500

    f

--- main.py
+++ main.py
@@ -25,3 +25,7 @@ def main():
     return RedirectResponse(url="/docs")
 
 app.include_router(rutas)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
